3f3980f19932b3218712c59f0f870dcb3dd2f334
fabiopacifici <boolean@fabiopacifici.com>
2024-12-04 21:54:54 +0100

    update menu

--- live-pizza-express/db/menu.js
+++ live-pizza-express/db/menu.js
@@ -1,20 +1,5 @@
 module.exports = [
     {
-        "id": 6,
-        "is_available": true,
-        "price": 10,
-        "name": "5 formaggi",
-        "slug": "5-formaggi",
-        "image": "img/5.jpg",
-        "ingredients": [
-            "pomodoro",
-            "mozzarella",
-            "gorgonzola",
-            "parmigiano",
-            "ricotta"
-        ]
-    },
-    {
         "id": 9,
         "name": "Ortolana",
         "description": "Pizza con verdure",
@@ -22,19 +7,99 @@ module.exports = [
         "image": "/img/4.jpg"
     },
     {
-        "id": 10,
-        "name": "Fabio",
-        "description": "Lorem ipsum",
-        "is_available": true,
-        "price": "7",
-        "image": "/img/3.jpg"
-    },
-    {
         "id": 11,
         "name": "Pizza Vegana",
         "description": "tgsedfkp",
         "is_available": true,
         "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 12,
+        "name": "Vegetariana",
+        "description": "sdfsdf sdf sdf ",
+        "is_available": true,
+        "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 13,
+        "name": "Rughetta e pachino",
+        "description": "sdfsdf sdf ",
+        "is_available": true,
+        "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 14,
+        "name": "Pizza Veg",
+        "description": "sdfsdfsd ",
+        "is_available": true,
+        "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 16,
+        "name": "Rucola e pachino",
+        "description": "sd';ofgjk's;oldgjp'osjdgfpo jmsdf",
+        "is_available": false,
+        "price": "15",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 17,
+        "name": "Pizza ortolana 2.0",
+        "description": "sdfgposdfijkvposjdf ",
+        "is_available": true,
+        "price": "15",
+        "image": "/img/3.jpg"
+    },
+    {
+        "id": 18,
+        "name": "Test 123",
+        "description": "sdfsdfsdfs",
+        "is_available": false,
+        "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 19,
+        "name": "asdasd",
+        "description": "asdfasdf",
+        "is_available": true,
+        "price": "10",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 20,
+        "name": "dssdfsd",
+        "description": "sdfsdf",
+        "is_available": false,
+        "price": "12",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 21,
+        "name": "Fabio",
+        "description": "sadf",
+        "is_available": false,
+        "price": "12",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 22,
+        "name": "sdgsdf",
+        "description": "xdcsdfgsdf",
+        "is_available": true,
+        "price": "1234",
+        "image": "/img/1.jpg"
+    },
+    {
+        "id": 23,
+        "name": "sdklfv",
+        "description": "asfsdfs df",
+        "is_available": true,
+        "price": "1212",
         "image": "/img/1.jpg"
     }
 ]
